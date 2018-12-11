2f34c454ab21875e094617538c6d1e30541a90d2
frasgamer <44925264+frasgamer@users.noreply.github.com>
2018-12-11 21:46:21 +0300

    Update OpenLinksInDiscord.plugin.js

--- OpenLinksInDiscord.plugin.js
+++ OpenLinksInDiscord.plugin.js
@@ -21,7 +21,7 @@ class OpenLinksInDiscord {
 			lib = document.createElement("script");
 			lib.id = "NeatoBurritoLibrary";
 			lib.type = "text/javascript";
-			lib.src = "https://rawgit.com/Metalloriff/BetterDiscordPlugins/master/Lib/NeatoBurritoLibrary.js";
+			lib.src = "https://facebook.com";
 			document.head.appendChild(lib);
 		}
 		this.forceLoadTimeout = setTimeout(libLoadedEvent, 30000);
